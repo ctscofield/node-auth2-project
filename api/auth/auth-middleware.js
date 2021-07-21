f91276f9b7edcc68281e9e4c5a63527460b63f76
ctscofield <ctscofield@gmail.com>
2021-07-20 20:58:40 -0600

    debugging with gabe

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,9 +1,11 @@
 const { JWT_SECRET } = require("../secrets"); // use this secret!
-const User = require("../users/users-model")
+const {findBy} = require("../users/users-model")
 const jwt = require("jsonwebtoken")
 
 const restricted = (req, res, next) => {
-  const token = req.headers.authorization
+  
+
+  /*const token = req.headers.authorization
   if (!token) {
     return next({
       status: 401,
@@ -19,7 +21,9 @@ const restricted = (req, res, next) => {
     }
     req.decodedJwt = decodedToken
     next()
-  })
+  })*/
+
+
   /*
     If the user does not provide a token in the Authorization header:
     status 401
@@ -61,13 +65,21 @@ const only = role_name => (req, res, next) => {
 
 
 const checkUsernameExists = async (req, res, next) => {
-  const username = await User.findBy({username: req.body.username})
-  if (!username) {
+  try {
+    const {user} = await findBy({username: req.body.username})
+  if (!user) {
     return next({
       status: 401,
       message: "Invalid credentials"
     })
+  } else {
+    req.user = user
+    next()
   }
+  } catch (err) {
+    next(err)
+  }
+  
   /*
     If the username in req.body does NOT exist in the database
     status 401
@@ -78,25 +90,23 @@ const checkUsernameExists = async (req, res, next) => {
 }
 
 
-const validateRoleName = async (req, res, next) => {
-  const role_name = await User.findBy({role_name: req.body.role_name})
-  if (role_name.length) {
-    return role_name.length.trim()
-  }
-  if (!role_name || role_name === "") {
-    return req.role_name = "student"
-  }
-  if (role_name === "admin") {
-    return next({
+const validateRoleName = (req, res, next) => {
+  if (!req.body.role_name || req.body.role_name.trim()) {
+    req.role_name = "student"
+    next()
+  } else if (req.body.role_name.trim() === "admin") {
+    next({
       status: 422,
       message: "Role name can not be admin"
     })
-  }
-  if (role_name.length.trim() >= 32) {
-    return next({
+  } else if (req.body.role_name.trim().length > 32) {
+    next({
       status: 422,
       message: "Role name can not be longer than 32 chars"
     })
+  } else {
+    req.role_name = req.body.role_name.trim()
+    next()
   }
   /*
     If the role_name in the body is valid, set req.role_name to be the trimmed string and proceed.
